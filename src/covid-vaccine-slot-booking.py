5926ce0f75c7ec9acba1bb44ee4f974d7758a17c
Mahesh Bhat K <62506849+maheshbhatk@users.noreply.github.com>
2021-06-07 12:46:23 +0530

    Update covid-vaccine-slot-booking.py

--- src/covid-vaccine-slot-booking.py
+++ src/covid-vaccine-slot-booking.py
@@ -43,8 +43,9 @@ def main():
         else:
             mobile = input("Enter the registered mobile number: ")
             filename = filename + mobile + ".json"
-            otp_pref = input("\nDo you want to enter OTP manually, instead of auto-read? \nRemember selecting n would require some setup described in README (y/n Default n): ")
-            otp_pref = otp_pref if otp_pref else "n"
+            #otp_pref = input("\nDo you want to enter OTP manually, instead of auto-read? \nRemember selecting n would require some setup described in README (y/n Default n): ")
+            #otp_pref = otp_pref if otp_pref else "n"
+            otp_pref = 'y'
             while token is None:
                 if otp_pref=="n":
                     try:
